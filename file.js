0a2bb8c022f5e3d29906112f645e9c08f936e1c7
rohan684 <dheerupandey684@gmail.com>
2021-11-27 23:28:08 +0530

    First Commit

---
+++ file.js
@@ -0,0 +1,36 @@
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+      mySidebar.style.display = 'none';
+    } else {
+      mySidebar.style.display = 'block';
+    }
+  }
+  
+  // Close the sidebar with the close button
+  function w3_close() {
+      mySidebar.style.display = "none";
+  }
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+  
+      if (scrollTop >= 150 ) {
+        $(".nav-container").addClass("solid-nav");
+      } else {
+        $(".nav-container").removeClass("solid-nav");
+      }
+    });
+  });
+  function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+  function side_bar_button(){
+    mySidebar.style.display = "none";
+    document.getElementById('id01').style.display='block'
+  }
